7eac2ae95f30e474f499cb81b9131d5058c27c00
Tiago Sousa <tiago.ssantos814@gmail.com>
2025-03-28 17:15:21 -0300

    mapeia area

---
+++ src/main/java/dev/tiagowakanda/produdoro/area/domain/domain/Area.java
@@ -0,0 +1,26 @@
+package dev.tiagowakanda.produdoro.area.domain.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Document(collection = "Area")
+public class Area {
+    @Id
+    private UUID idArea;
+    private String nome;
+    private String descricao;
+    @Indexed
+    private UUID idUsuario;
+
+}
