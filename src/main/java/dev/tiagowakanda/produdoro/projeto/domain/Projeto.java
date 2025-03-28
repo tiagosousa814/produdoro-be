4eb07c724d858d2a96557aadfd52cb24a7e52b21
Tiago Sousa <tiago.ssantos814@gmail.com>
2025-03-28 17:07:27 -0300

    mapeia projeto

---
+++ src/main/java/dev/tiagowakanda/produdoro/projeto/domain/Projeto.java
@@ -0,0 +1,27 @@
+package dev.tiagowakanda.produdoro.projeto.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Document(collection = "Projeto")
+public class Projeto {
+    @Id
+    private UUID idProjeto;
+    private String nome;
+    private String descricao;
+    @Indexed
+    private UUID idUsuario;
+    @Indexed
+    private UUID idArea;
+}
